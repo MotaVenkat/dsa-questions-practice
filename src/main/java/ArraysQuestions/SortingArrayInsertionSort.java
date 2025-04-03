d62708ea26d4e39c6b9d7760189f894a27ced7e5
MotaVenkat <154257671+MotaVenkat@users.noreply.github.com>
2025-04-03 07:28:35 +0530

    Array questions

--- src/main/java/ArraysQuestions/SortingArrayInsertionSort.java
+++ src/main/java/ArraysQuestions/SortingArrayInsertionSort.java
@@ -5,18 +5,19 @@ import java.util.Arrays;
 public class SortingArrayInsertionSort {
     public static void main(String[] args) {
         int [] a ={3,4,1,6};
-
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<a.length;j++){
-                if(a[j]>a[i]){
+        for (int i = 1; i <a.length ; i++) {
+            for (int j = i; j > 0 ; j--) {
+                if(a[j] < a[j-1]){
                     int temp= a[j];
-                    a[j]=a[i];
-                    a[i]=temp;
-                }
+                     a[j]=a[j-1];
+                     a[j-1]=temp;
 
+                }
             }
 
         }
+
+
         System.out.println(Arrays.toString(a));
     }
 }
---
+++ src/main/java/ArraysQuestions/SpiralMatrixquestion.java
@@ -0,0 +1,16 @@
+package ArraysQuestions;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class SpiralMatrixquestion {
+    public static void main(String[] args) {
+        int [][]s={{1,2,3},{4,5,6},{7,8,9}};
+        int i=0;
+        int j=0;
+        /*while(){
+            System.out.println(s[i][j]);
+        }*/
+    }
+
+
+}
